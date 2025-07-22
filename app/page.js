0c540384084e21fa987584a893d6282cfb61794d
prabhu-woyce <pparmar@woyce.io>
2025-07-22 11:53:08 +0530

    #update Add shadcn com

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
 
-"use client"
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
 import { Button } from ".././components/ui/button"
 
 
 export default function Home() {
-  const { setTheme } = useTheme()
   return (
     <div>
       <div>
