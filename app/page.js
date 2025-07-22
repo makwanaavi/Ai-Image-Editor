6fc60b7c521d9f64a3f3ae131793220ba261815e
prabhu-woyce <pparmar@woyce.io>
2025-07-22 11:26:18 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ import { Button } from ".././components/ui/button"
 export default function Home() {
   return (
     <div>
-       <div className="flex flex-wrap items-center gap-2 md:flex-row">
+       <div className="flex flex-wrap items-center gap-2 md:flex-row bg-black">
       <br />
       <Button variant="primary">Button</Button>
+      <Button variant="glass">Button</Button>
     </div>
     </div>
   );
