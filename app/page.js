ea3e0e41a01c8e9dccea2522330568ac55e732e3
prabhu-woyce <pparmar@woyce.io>
2025-07-22 11:13:11 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+
+import { Button } from ".././components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+       <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <br />
+      <Button variant="primary">Button</Button>
+    </div>
+    </div>
+  );
+}
